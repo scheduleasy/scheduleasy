8f28479ce2927c2fbda262f7988d1642a0324340
Eugênio Moreira <eugeniojava@outlook.com>
2022-11-10 23:03:42 -0300

    bootstrap project

---
+++ src/test/java/app/scheduleasy/scheduleasyapi/ScheduleasyApiApplicationTests.java
@@ -0,0 +1,11 @@
+package app.scheduleasy.scheduleasyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleasyApiApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
