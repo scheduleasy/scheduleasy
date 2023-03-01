c88428a5b4801b192af6de4d9f10f28a1709582b
Eugênio Moreira <eugeniojava@outlook.com>
2023-03-01 11:27:27 -0300

    feat(api): update project to Kotlin and Gradle

---
+++ api/build.gradle.kts
@@ -0,0 +1,43 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("io.spring.dependency-management") version "1.1.0"
+    id("org.jetbrains.kotlinx.kover") version "0.6.1"
+    id("org.jlleitschuh.gradle.ktlint") version "11.2.0"
+    id("org.springframework.boot") version "3.0.3"
+    kotlin("jvm") version "1.7.22"
+    kotlin("plugin.spring") version "1.7.22"
+}
+
+group = "app.scheduleasy"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+kover {
+    htmlReport {
+        onCheck.set(true)
+    }
+}
---
+++ api/src/test/kotlin/app/scheduleasy/api/ApiApplicationTests.kt
@@ -0,0 +1,11 @@
+package app.scheduleasy.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
